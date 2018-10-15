0c311a8412965d5fb92f920e9bb610b9646f487e
new people <43990524+antonescu@users.noreply.github.com>
2018-10-15 12:39:16 +0100

    Create div.py

---
+++ div.py
@@ -0,0 +1,18 @@
+>>> x=20
+>>> a=15
+>>> dif=x-a
+>>> print ("diference between " ,x, "and" ,a, "is:", dif)
+diference between  20 and 15 is: 5
+>>> print ("difference between", x,"and", a,"is:",dif)
+difference between 20 and 15 is: 5
+>>> 
+>>> d=10
+>>> b=2
+>>> div=10/2
+>>> print ("divide betwen " ,d, "and" ,b, "is:", div)
+divide betwen  10 and 2 is: 5.0
+>>> c=5
+>>> e=5
+>>> mul=5*5
+("multiple between" ,c, "and" ,e, "is:",mul)
+('multiple between', 5, 'and', 5, 'is:', 25)
